923668a8d4b5c3482fa771f70bf1ce9bd6bb1d5b
unknown <weslleysferreira14@gmail.com>
2026-06-19 18:52:14 -0300

    Atualizandop arquivos da Franciele com rota atualizada

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout.jsx";
+import { Home } from "./pages/Home";
+import { ResultadosPesquisa } from "./pages/ResultadosPesquisa";
+import { CadastroUsuario } from "./pages/CadastroUsuario";
+import { Login } from "./pages/Login";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<CadastroUsuario />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* Futuramente, novas páginas serão adicionadas aqui */}
+          <Route path="resultados" element={<ResultadosPesquisa />} />
+          <Route path="contact" element={<div className="p-20 text-center text-xl">Contato (Em breve)</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
